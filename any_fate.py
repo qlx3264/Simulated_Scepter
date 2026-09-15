129ae9e88c2258b6e1d388f42fb463609bd814c2
歪比巴卜 <ozfire1234@163.com>
2026-09-15 23:09:36 +0800

    Update any_fate.py

--- any_fate.py
+++ any_fate.py
@@ -239,7 +239,7 @@ class AnyFateUniverse(SimulatedUniverse):
                         bean = self.check("bean", 0.1536,0.7056)
                         skill_num = match_skill_numbers_in_region(self.get_screen())
                         CUS_LOGGER.debug(f"当前秘技点数量：{skill_num}")
-                        self.skill_num = skill_num if (skill_num is not None) else 99
+                        self.skill_num = skill_num if (skill_num is not None) else 5
                         # 秘技未施放时，秘技点超过1，或者秘技点为1且当前区域无小怪，则施放银狼秘技
                         if not bean and (self.skill_num >= 2 or (self.skill_num == 1 and ("战斗" not in self.area or pig))):
                             key_mouse_manager.press('e')
@@ -250,7 +250,6 @@ class AnyFateUniverse(SimulatedUniverse):
                             key_mouse_manager.press('e')
                             CUS_LOGGER.debug("已解除银狼秘技")
                             key_mouse_manager.sleep(0.6)
-                        self.skill_num = skill_num if (skill_num is not None) else None
                 key_mouse_manager.wait()
                 battle_map_root = os.path.join(PATHS["image"], "nmaps")
                 if (("战斗" in self.area or "精英" in self.area or "首领" in self.area)
