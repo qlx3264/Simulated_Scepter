656a3d551a837ac99dceccebd046eb6a5ddb2440
歪比巴卜 <ozfire1234@163.com>
2026-09-16 20:24:42 +0800

    默认值改为0

--- iron_blood.py
+++ iron_blood.py
@@ -34,7 +34,7 @@ class IronBloodUniverse(AnyFateUniverse):
         self.second_plane_count = self.opt.get("second_plane", 31)
         self.first_plane_min_weight = self.opt.get("first_plane_min_weight", 6)
         self.third_plane_pause_count = self.opt.get("third_plane_pause_count", 0)
-        self.boss_before_pause_count = self.opt.get("boss_before_pause_count", 39)
+        self.boss_before_pause_count = self.opt.get("boss_before_pause_count", 0)
         self.fate = "毁灭"
         self.my_fate = config.fates.index(self.fate)
         self.tk = text_keys(self.my_fate)
