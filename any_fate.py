f9ce1369b84d31f1f29f327533d6969d71d3c627
qlx3264 <yumeixiu23@gmail.com>
2026-09-20 21:02:10 +0800

    修复判断错误导致的秘技点异常消耗

--- any_fate.py
+++ any_fate.py
@@ -291,9 +291,11 @@ class AnyFateUniverse(SimulatedUniverse):
                             if cast:
                                 key_mouse_manager.press('e')
                                 CUS_LOGGER.debug("已施放银狼秘技")
+                                key_mouse_manager.sleep(0.6)
                             elif release:
                                 key_mouse_manager.press('e')
                                 CUS_LOGGER.debug("已解除银狼秘技")
+                                key_mouse_manager.sleep(0.6)
                 key_mouse_manager.wait()
                 battle_map_root = os.path.join(PATHS["image"], "nmaps")
                 if (("战斗" in self.area or "精英" in self.area or "首领" in self.area)
